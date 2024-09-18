915584e6c25b0653b87169cfb8e99de2f524960c
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-09-17 20:40:51 -0400

    audio update

--- script.js
+++ script.js
@@ -41,13 +41,60 @@ window.onload = function() {
         white: "var(--terminal_white)"
     };
 
+    // audio for keypresses and key releases
+    // audio files from kbs.im
+    const audioElements = {
+        backspaceKeyPress: new Audio("audio/press/BACKSPACE.mp3"),
+        enterKeyPress: new Audio("audio/press/ENTER.mp3"),
+        spacebarKeyPress: new Audio("audio/press/SPACE.mp3"),
+        genericKeyPress: new Audio("audio/press/GENERIC_R0.mp3"),
+        genericKeyPress1: new Audio("audio/press/GENERIC_R1.mp3"),
+        genericKeyPress2: new Audio("audio/press/GENERIC_R2.mp3"),
+        genericKeyPress3: new Audio("audio/press/GENERIC_R3.mp3"),
+        genericKeyPress4: new Audio("audio/press/GENERIC_R4.mp3"),
+        backspaceKeyRelease: new Audio("audio/release/BACKSPACE.mp3"),
+        enterKeyRelease: new Audio("audio/release/ENTER.mp3"),
+        spacebarKeyRelease: new Audio("audio/release/SPACE.mp3"),
+        genericKeyRelease: new Audio("audio/release/GENERIC.mp3"),
+        weAreAudio: new Audio("audio/we_are.mp3")
+    };
+
+    // detect key input then play sound accordingly
+    const soundMap = {
+        'Backspace': {
+            keyDown: audioElements.backspaceKeyPress,
+            keyUp: audioElements.backspaceKeyRelease
+        },
+        'Enter': {
+            keyDown: audioElements.enterKeyPress,
+            keyUp: audioElements.enterKeyRelease
+        },
+        ' ': {
+            keyDown: audioElements.spacebarKeyPress,
+            keyUp: audioElements.spacebarKeyRelease
+        }
+    };
+    
+    function playSound(event, isKeyDown) {
+        const key = event.key;
+        const sound = soundMap[key];
+        
+        if (sound) {
+            (isKeyDown ? sound.keyDown : sound.keyUp)?.play();
+        } else {
+            (isKeyDown ? audioElements.genericKeyPress : audioElements.genericKeyRelease)?.play();
+        }
+    }
+    
+
+
     const commands = {
         "echo": (args) => args.join(" "),
         "cls": () => {
             terminal.innerHTML = splashText;
             return "";
         },
-        "help": () => "Commands:\n\necho\ncls\nhelp\ncd\ncolor",
+        "help": () => "Commands:\n\nhelp          -> list of commands\necho {text}   -> repeats input\ncls           -> clears terminal\ncd {html}     -> redirects to the html page\ncolor {color} -> changes terminal color\ndir           -> shows list of files\nexit          -> closes tab\n",
         "cd": (args) => {
             if (args.length > 0) {
                 const destination = args[0];
@@ -56,7 +103,22 @@ window.onload = function() {
                 return "The system cannot find the path specified.";
             }
         },
-        "onepiece.exe": () => "WEAREWEGOSUNNY!",
+        "onepiece.exe": () => {
+            terminal.innerHTML += "now playing: 🎶 We Are 🎶\n";
+            audioElements.weAreAudio.play(); // Play the BEST ANIME OPENING OF ALL TIME
+            return "";
+        },
+        "virus.exe": () => {
+        const virusText = "⠀⠀⠀⠀⠀⣀⡴⠖⠒⠒⢒⣒⡖⠒⠒⠒⠒⠒⠒⠶⠶⠤⣤⣀⣀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⣴⠋⠀⠀⠤⣪⣝⡲⠯⠭⠥⠀⠀⠀⠀⠀⣀⣐⣒⡒⠉⠙⢦⡀⠀⠀\n⠀⠀⠀⣼⠃⠀⠈⠰⠫⠋⣀⣀⣀⣀⠀⠃⠀⠀⠀⠸⠀⠀⠀⠈⠆⠀⠀⢧⠠⠀\n⠀⣠⡾⠁⠀⡀⠠⠄⢰⣿⠿⠿⢯⣍⣙⣶⠀⠀⢀⣠⣶⣾⣿⠶⠆⠤⠤⢜⣷⡄\n⡾⢻⢡⡞⠋⣽⠛⠲⠤⡤⠴⠋⠀⠀⠉⠁⠀⠀⠈⣿⠁⠀⢀⣀⣠⠶⠶⣽⣵⣿\n⣇⢠⢸⡥⠶⣟⠛⠶⣤⣀⠀⠀⠀⢲⡖⣂⣀⠀⠀⠈⢳⣦⡀⠉⠉⣽⡄⠰⣻⣿\n⠙⣮⡪⠁⠀⠻⣶⣄⣸⣍⠙⠓⠶⣤⣥⣉⣉⠀⠠⠴⠋⠁⣈⣥⣴⣿⡇⠈⣽⠃\n⠀⠈⢻⡄⠀⠀⠙⣆⢹⡟⠷⣶⣤⣇⣀⠉⠙⡏⠉⣻⡟⢉⣹⣅⣼⣿⡇⠀⡏⠀\n⠀⠀⠀⠻⣄⠀⠀⠈⠻⢦⡀⠀⣽⠉⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⡇⠀\n⠀⠀⠀⠀⠙⢦⣀⠄⡀⢄⡙⠻⠧⣤⣀⣀⣿⠀⠀⣿⢀⣼⣃⣾⣼⠟⠁⠀⡇⠀\n⠀⠀⠀⠀⠀⠀⠉⠓⢮⣅⡚⠵⣒⡤⢄⣉⠉⠉⠉⠉⠉⠉⠉⢀⡠⠀⠀⠀⣷⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠳⢦⣄⡉⠙⠛⠃⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⡿⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⠶⢤⣤⣀⣀⣀⣀⣀⣀⡤⠞⠁⠀\n";
+            
+            terminal.innerHTML += "your pc is going to explode now!\n";
+            terminal.innerHTML += virusText;
+            setTimeout(() => {
+                close; 
+            }, 5000);
+        },
+        "dir": () => "2024-06-16  12:33 PM    {DIR}      about\n2024-07-14   4:12 PM    {DIR}      projects\n2024-08-22  12:33 PM    {DIR}      contact\n2024-09-17  11:54 AM    {EXE}      virus.exe\n",
+        "exit": () => {close();},
         "color": (args) => {
                 if (args.length > 0) {
                     const colorName = args[0].toLowerCase();
@@ -78,6 +140,7 @@ window.onload = function() {
 
     // Handle input
     input.addEventListener("keydown", function(event) {
+        playSound(event, true); // Play sound on keydown
         if (event.key === "Enter") {
             const userInput = input.value.trim();
             terminal.innerHTML += currentPrompt + userInput + "\n";
@@ -85,6 +148,10 @@ window.onload = function() {
             input.value = "";
             terminal.scrollTop = terminal.scrollHeight;
         }
+    });
+
+    input.addEventListener("keyup", function(event) {
+        playSound(event, false); // Play sound on keyup
     });
 
     function processCommand(userInput) {
