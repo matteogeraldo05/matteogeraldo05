ab992bb4a36c115d1660b752c5e1af327d3ca0ca
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2024-09-20 13:11:13 -0400

    sidebar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // change the title to matteogeraldo05
 function normalTitle() {
-    document.title = "matteogeraldo05";
+    document.title = "Matteogeraldo05";
 }
 
 // change the title to this
@@ -19,14 +19,14 @@ window.onload = function() {
     const terminalContainer = document.getElementById("terminal");
   
     const splashText = "MatteOS [Version 11.26]\n(c) MatteOS. All rights reserved.\n\n";
-    let currentPrompt  = "C:\\";
+    let currentPrompt = "C:\\";
 
     // splash text
     terminal.innerHTML += splashText;
     // autoscroll down
     terminal.scrollTop = terminal.scrollHeight; 
 
-    // wherever on the terminal you click it defaults to the inpput line
+    // wherever on the terminal you click it defaults to the input line
     terminalContainer.addEventListener("click", function() {
         input.focus();
     });
@@ -42,7 +42,6 @@ window.onload = function() {
     };
 
     // audio for keypresses and key releases
-    // audio files from kbs.im
     const audioElements = {
         backspaceKeyPress: new Audio("audio/press/BACKSPACE.mp3"),
         enterKeyPress: new Audio("audio/press/ENTER.mp3"),
@@ -57,15 +56,15 @@ window.onload = function() {
 
     // detect key input then play sound accordingly
     const soundMap = {
-        'Backspace': {
+        "Backspace": {
             keyDown: audioElements.backspaceKeyPress,
             keyUp: audioElements.backspaceKeyRelease
         },
-        'Enter': {
+        "Enter": {
             keyDown: audioElements.enterKeyPress,
             keyUp: audioElements.enterKeyRelease
         },
-        ' ': {
+        " ": {
             keyDown: audioElements.spacebarKeyPress,
             keyUp: audioElements.spacebarKeyRelease
         }
@@ -81,58 +80,86 @@ window.onload = function() {
             (isKeyDown ? audioElements.genericKeyPress : audioElements.genericKeyRelease)?.play();
         }
     }
-    
-
 
     const commands = {
+        //repeat user input
         "echo": (args) => args.join(" "),
+        //clear screen
         "cls": () => {
             terminal.innerHTML = splashText;
             return "";
         },
-        "help": () => "Commands:\n\nhelp          -> list of commands\necho {text}   -> repeats input\ncls           -> clears terminal\ncd {html}     -> redirects to the html page\ncolor {color} -> changes terminal color\ndir           -> shows list of files\nexit          -> closes tab\n",
+        //list commands
+        "help": () => "Commands:\n\nhelp          -> list of commands\n"+
+        "echo {text}   -> repeats input\n"+
+        "cls           -> clears terminal\n"+
+        "cd {html}     -> redirects to the html page\n"+
+        "color {color} -> changes terminal color\n"+
+        "dir           -> shows list of files\n"+
+        "exit          -> closes tab\n",
+        //change directory
         "cd": (args) => {
-            if (args.length > 0) {
+            if ((args.length > 0) && (args == "about" || args == "projects" || args == "contact")) {
                 const destination = args[0];
                 window.location.href = destination + ".html";
             } else {
                 return "The system cannot find the path specified.";
             }
         },
+        //one piece easter egg
         "onepiece.exe": () => {
             terminal.innerHTML += "now playing: 🎶 We Are 🎶\n";
             audioElements.weAreAudio.play(); // Play the BEST ANIME OPENING OF ALL TIME
             return "";
         },
+        //virus easter egg
         "virus.exe": () => {
-        const virusText = "⠀⠀⠀⠀⠀⣀⡴⠖⠒⠒⢒⣒⡖⠒⠒⠒⠒⠒⠒⠶⠶⠤⣤⣀⣀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⣴⠋⠀⠀⠤⣪⣝⡲⠯⠭⠥⠀⠀⠀⠀⠀⣀⣐⣒⡒⠉⠙⢦⡀⠀⠀\n⠀⠀⠀⣼⠃⠀⠈⠰⠫⠋⣀⣀⣀⣀⠀⠃⠀⠀⠀⠸⠀⠀⠀⠈⠆⠀⠀⢧⠠⠀\n⠀⣠⡾⠁⠀⡀⠠⠄⢰⣿⠿⠿⢯⣍⣙⣶⠀⠀⢀⣠⣶⣾⣿⠶⠆⠤⠤⢜⣷⡄\n⡾⢻⢡⡞⠋⣽⠛⠲⠤⡤⠴⠋⠀⠀⠉⠁⠀⠀⠈⣿⠁⠀⢀⣀⣠⠶⠶⣽⣵⣿\n⣇⢠⢸⡥⠶⣟⠛⠶⣤⣀⠀⠀⠀⢲⡖⣂⣀⠀⠀⠈⢳⣦⡀⠉⠉⣽⡄⠰⣻⣿\n⠙⣮⡪⠁⠀⠻⣶⣄⣸⣍⠙⠓⠶⣤⣥⣉⣉⠀⠠⠴⠋⠁⣈⣥⣴⣿⡇⠈⣽⠃\n⠀⠈⢻⡄⠀⠀⠙⣆⢹⡟⠷⣶⣤⣇⣀⠉⠙⡏⠉⣻⡟⢉⣹⣅⣼⣿⡇⠀⡏⠀\n⠀⠀⠀⠻⣄⠀⠀⠈⠻⢦⡀⠀⣽⠉⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⡇⠀\n⠀⠀⠀⠀⠙⢦⣀⠄⡀⢄⡙⠻⠧⣤⣀⣀⣿⠀⠀⣿⢀⣼⣃⣾⣼⠟⠁⠀⡇⠀\n⠀⠀⠀⠀⠀⠀⠉⠓⢮⣅⡚⠵⣒⡤⢄⣉⠉⠉⠉⠉⠉⠉⠉⢀⡠⠀⠀⠀⣷⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠳⢦⣄⡉⠙⠛⠃⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⡿⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⠶⢤⣤⣀⣀⣀⣀⣀⣀⡤⠞⠁⠀\n";
-            
+            const virusText = "⠀⠀⠀⠀⠀⣀⡴⠖⠒⠒⢒⣒⡖⠒⠒⠒⠒⠒⠒⠶⠶⠤⣤⣀⣀⠀⠀⠀⠀⠀\n" +
+                "⠀⠀⠀⠀⣴⠋⠀⠀⠤⣪⣝⡲⠯⠭⠥⠀⠀⠀⠀⠀⣀⣐⣒⡒⠉⠙⢦⡀⠀⠀\n" +
+                "⠀⠀⠀⣼⠃⠀⠈⠰⠫⠋⣀⣀⣀⣀⠀⠃⠀⠀⠀⠸⠀⠀⠀⠈⠆⠀⠀⢧⠠⠀\n" +
+                "⠀⣠⡾⠁⠀⡀⠠⠄⢰⣿⠿⠿⢯⣍⣙⣶⠀⠀⢀⣠⣶⣾⣿⠶⠆⠤⠤⢜⣷⡄\n" +
+                "⡾⢻⢡⡞⠋⣽⠛⠲⠤⡤⠴⠋⠀⠀⠉⠁⠀⠀⠈⣿⠁⠀⢀⣀⣠⠶⠶⣽⣵⣿\n" +
+                "⣇⢠⢸⡥⠶⣟⠛⠶⣤⣀⠀⠀⠀⢲⡖⣂⣀⠀⠀⠈⢳⣦⡀⠉⠉⣽⡄⠰⣻⣿\n" +
+                "⠙⣮⡪⠁⠀⠻⣶⣄⣸⣍⠙⠓⠶⣤⣥⣉⣉⠀⠠⠴⠋⠁⣈⣥⣴⣿⡇⠈⣽⠃\n" +
+                "⠀⠈⢻⡄⠀⠀⠙⣆⢹⡟⠷⣶⣤⣇⣀⠉⠙⡏⠉⣻⡟⢉⣹⣅⣼⣿⡇⠀⡏⠀\n" +
+                "⠀⠀⠀⠻⣄⠀⠀⠈⠻⢦⡀⠀⣽⠉⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⡇⠀\n" +
+                "⠀⠀⠀⠀⠙⢦⣀⠄⡀⢄⡙⠻⠧⣤⣀⣀⣿⠀⠀⣿⢀⣼⣃⣾⣼⠟⠁⠀⡇⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠉⠓⢮⣅⡚⠵⣒⡤⢄⣉⠉⠉⠉⠉⠉⠉⠉⠉⢀⡠⠀⠀⣷⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠳⢦⣄⡉⠙⠛⠃⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⡿⠀\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⠶⢤⣤⣀⣀⣀⣀⣀⣀⡤⠞⠁⠀\n";
+                
             terminal.innerHTML += "your pc is going to explode now!\n";
             terminal.innerHTML += virusText;
             setTimeout(() => {
                 close(); 
             }, 5000);
         },
-        "dir": () => "2024-06-16  12:33 PM    {DIR}      about\n2024-07-14   4:12 PM    {DIR}      projects\n2024-08-22  12:33 PM    {DIR}      contact\n2024-09-17  11:54 AM    {EXE}      virus.exe\n",
+        //list directory
+        "dir": () => "2024-06-16  12:33 PM    {DIR}      about\n" +
+                     "2024-07-14   4:12 PM    {DIR}      projects\n" +
+                     "2024-08-22  12:33 PM    {DIR}      contact\n" +
+                     "2024-09-17  11:54 AM    {EXE}      virus.exe\n",
+        //close site
         "exit": () => {close();},
+        //change terminal color
         "color": (args) => {
-                if (args.length > 0) {
-                    const colorName = args[0].toLowerCase();
-                    if (colors[colorName]) {
-                        const selectedColor = colors[colorName];
-                        // Apply color to terminal, prompt, and input
-                        terminal.style.color = selectedColor;
-                        prompt.style.color = selectedColor;
-                        input.style.color = selectedColor;
-                        return `Text color changed to ${colorName}.`;
-                    } else {
-                        return `Invalid color. Available colors: ${Object.keys(colors).join(", ")}.`;
-                    }
+            if (args.length > 0) {
+                const colorName = args[0].toLowerCase();
+                if (colors[colorName]) {
+                    const selectedColor = colors[colorName];
+                    // Apply color to terminal, prompt, and input
+                    terminal.style.color = selectedColor;
+                    prompt.style.color = selectedColor;
+                    input.style.color = selectedColor;
+                    return `Text color changed to ${colorName}.`;
                 } else {
-                    return "Please specify a color.";
+                    return `Invalid color. Available colors: ${Object.keys(colors).join(", ")}.`;
                 }
+            } else {
+                return "Please specify a color.";
             }
-        };
+        }
+    };
 
     // Handle input
     input.addEventListener("keydown", function(event) {
@@ -164,4 +191,14 @@ window.onload = function() {
             terminal.innerHTML += `${userInput} is not recognized as a command.\n`;
         }
     }
+
+    // Sidebar functionality
+    const sidebar = document.getElementById("sidebar");
+    // Fade in each button with a delay
+    const icons = document.querySelectorAll(".sidebar-buttons input");
+    icons.forEach((icon, index) => {
+        setTimeout(() => {
+            icon.classList.add("fade-in");
+        }, index * 150); // stagger the fade-in by 150ms
+    });
 };
